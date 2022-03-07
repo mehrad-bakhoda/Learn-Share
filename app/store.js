a2e0c917f2d11604d3cf21792cd034d158e690bf
BlueShdw <no2.nemesis@gmail.com>
2022-03-07 16:41:06 +0330

    removing filters store

--- app/store.js
+++ app/store.js
@@ -1,11 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import tokenReducer from "./features/tokenSlice";
-import filterReducer from "./features/filterSlice";
 
 const store = configureStore({
   reducer: {
     token: tokenReducer,
-    filter: filterReducer,
   },
 });
 export default store;
